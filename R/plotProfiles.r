eb5bdea08bbcfb17aa265a519b108471b09fc003
dkneis <david.kneis@tu-dresden.de>
2016-03-09 09:33:18 +0000

    REV: Revised method to plot vertical profiles

--- R/plotProfiles.r
+++ R/plotProfiles.r
@@ -4,15 +4,20 @@
 #'
 #' @inheritParams plot3d
 #' @param obs Data frame of observations. See notes below.
-#' @param combine Combine data for all times in a single plot?
-#' @param nc Maximum number of columns in layout. Only used if \code{combine} is
-#'   \code{FALSE}. The default of \code{NULL} puts all plots in a single row.
 #' @param pos Keyword specifying the position of the legend.
 #' @param xlim Limits for the value axis (x). Computed if \code{NULL}.
 #' @param ylim Limits for the depth axis (y). Computed if \code{NULL}.
+#' @param width Plot width in inch.
+#' @param height Plot height in inch.
+#' @param pointsize Font size.
+#' @param dir Output directory for generated graphics.
+#' @param fmt Graphics format, either 'svg' or 'pdf'.
+#' @param prefix Prefix put before the value of \code{name} when constructing
+#'   file names.
+#' @param replace If \code{FALSE} existing output files result in an error.
 #' @param ... Additional arguments to be passed to \code{legend}.
 #'
-#' @return \code{NULL}
+#' @return A vector holding the names of the generated graphics files.
 #'
 #' @note The data frame of observations passed as \code{obs} needs to
 #'   have 3 columns named 'time', 'depth', and 'value'. If the times of
@@ -21,8 +26,6 @@
 #'   'time' column must be of the same type as the return value of \code{timeconv}.
 #'   It is OK if the 'depth' and 'value' columns contain \code{NA} only and in
 #'   that case, just the simulated data are plotted.
-#'   The \code{layout} mechanism is used to arrange figures if \code{combine} is
-#'   \code{FALSE}, thus, nesting within other multi-figure setups won't work.
 #'
 #' @author David Kneis \email{david.kneis@@tu-dresden.de}
 #'
@@ -36,12 +39,14 @@
 #' colnames(dyn)= c("time", paste("X",1:nrow(gr),sep="."))
 #' obs= data.frame(time=c(1,1,2,2,2), depth=c(0.03, 0.04, 0.03, 0.04, 0.05))
 #' obs$value= runif(nrow(obs))
-#' plotProfiles("X", dyn, gr, obs, timeconv=function(x){x})
+#' plotProfiles("X", dyn, gr, obs, timeconv=function(x){x}, dir=tempdir())
 
 plotProfiles= function(name, dyn, gr, obs, xlab="mmol/L", ylab="depth",
-  xlim=NULL, ylim=NULL, combine=TRUE, nc=NULL,
+  xlim=NULL, ylim=NULL, 
   pos="top",
-  timeconv=function(x){ISOdatetime(1970,1,1,0,0,0)+x}, ...)
+  timeconv=function(x){ISOdatetime(1970,1,1,0,0,0)+x},
+  width=4, height=4, pointsize=10,
+  dir=".", fmt="svg", prefix="profile", replace=TRUE, ...)
 {
   obsNames= c("time","depth","value")
   if (!all(obsNames %in% names(obs)))
@@ -61,31 +66,25 @@ plotProfiles= function(name, dyn, gr, obs, xlab="mmol/L", ylab="depth",
     yrng= range(c(d, -obs$depth))
   else
     yrng= ylim
-  if (combine) {
-    plot(x=0, y=0, xlim=xrng, ylim=yrng, type="n", bty="n", xlab=xlab, ylab=ylab)
-    for (i in 1:length(it)) {
-      lines(m[i,], d, lty=1, col=i)
-      k= which(obs$time == times[i])
-      points(obs$value[k], -obs$depth[k], col=i)
-    }
-    legend(pos, bty="n", lty=1, col=1:length(it), legend=times, ...)
-  } else {
-    if (is.null(nc))
-      layout(matrix(1:length(it), nrow=1))
+  out=c()
+  for (i in 1:length(it)) {
+    ofile= paste0(dir,"/",prefix,name,"_",i,".",fmt)
+    out= c(out, ofile)
+    if ((!replace) && file.exists(ofile))
+      stop("file '",ofile,"' already exists")
+    if (fmt=="svg")
+      svg(ofile, width=width, height=height, pointsize=pointsize)
+    else if (fmt=="pdf")
+      pdf(ofile, width=width, height=height, pointsize=pointsize)
     else
-      nr= ceiling(length(it)/nc)
-      layout(matrix(1:(nr*nc), ncol=nc, byrow=TRUE))
-    for (i in 1:length(it)) {
-      label= (i == (nr-1)*nc+1) # axis labels only in lower left corner
-      plot(x=0, y=0, xlim=xrng, ylim=yrng, type="n", bty="n",
-        xlab=ifelse(label,xlab,""), ylab=ifelse(label,ylab,""))
-      lines(m[i,], d)
-      k= which(obs$time == times[i])
-      points(obs$value[k], -obs$depth[k])
-      legend(pos, bty="n", legend=times[i], ...)
-    }
-    layout(1)
+      stop("graphics format '",fmt,"' not supported")
+    plot(x=0, y=0, xlim=xrng, ylim=yrng, type="n", bty="n", xlab=xlab, ylab=ylab)
+    lines(m[i,], d)
+    k= which(obs$time == times[i])
+    points(obs$value[k], -obs$depth[k])
+    legend(pos, bty="n", legend=times[i], ...)
+    graphics.off()
   }
-  return(invisible(NULL))
+  return(out)
 }
 
